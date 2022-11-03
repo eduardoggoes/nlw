c93913d4c94ee42f2baa23b9b286dca0442c64f7
Eduardo Goes <dugoes@gmail.com>
2022-11-03 14:19:38 -0300

    aula 4 + refatoração do código

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 function createGame(player1, hour, player2) {
   return `
     <li>
-        <img src="./assests/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
         <strong>${hour}</strong>
-        <img src="./assests/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
     </li>
   `;
 }
 
+let delay = -0.4;
 function createCard(date, day, games) {
+  delay = 0 + 0.4;
   return `
-        <div class="card">
+        <div class="card" style="animation-delay: ${delay}s">
             <h2>${date} <span>${day}</span></h2>
             <ul>
                 ${games}
@@ -19,28 +21,18 @@ function createCard(date, day, games) {
     `;
 }
 
-document.querySelector("#app").innerHTML = `
-    <header>
-        <img src="./assests/logo.svg" alt="Logo NLW" />
-    </header>
-    <main id="cards">
-        ${createCard(
-          "24/11",
-          "quinta-feira",
-          createGame("switzerland", "07:00", "cameroon") +
-            createGame("uruguay", "10:00", "southKorea") +
-            createGame("portugal", "13:00", "ghana") +
-            createGame("brazil", "16:00", "serbia")
-        )}
-        ${createCard(
-          "28/11",
-          "segunda-feira",
-          createGame("brazil", "13:00", "switzerland")
-        )}
-        ${createCard(
-          "02/12",
-          "sexta-feira",
-          createGame("cameroon", "16:00", "brazil")
-        )}
-    </main>
-`;
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta-feira",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "southKorea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda-feira",
+    createGame("brazil", "13:00", "switzerland")
+  ) +
+  createCard("02/12", "sexta-feira", createGame("cameroon", "16:00", "brazil"));
